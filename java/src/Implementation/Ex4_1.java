fbcc4182cebf1b5ad7979fd6d232e4d69b65c7d6
OOHYEE <azss0470@gmail.com>
2021-08-14 19:31:32 +0900

    [Implementation] 상하좌우 책풀이 추가

--- java/src/Implementation/Ex4_1.java
+++ java/src/Implementation/Ex4_1.java
@@ -3,7 +3,6 @@ package Implementation;
 import java.util.Scanner;
 
 public class Ex4_1 {
-
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     static int x = 0;
@@ -23,7 +22,6 @@ public class Ex4_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        int[][] map = new int[n][n];
         sc.nextLine(); // 버퍼 지우기
         String str = sc.nextLine();
         for(int i=0; i<str.length(); i++) {
@@ -43,5 +41,41 @@ public class Ex4_1 {
             }
         }
         System.out.println((x+1) + " " + (y+1));
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String[] plans = sc.nextLine().split(" ");
+        int x = 1, y = 1;
+
+        //         왼쪽, 오른쪽, 위, 아래
+        int[] dx = {0,  0,   -1,  1};
+        int[] dy = {-1, 1,    0,  0};
+        char[] moveTypes = {'L', 'R', 'U', 'D'};
+
+        for(int i=0; i<plans.length; i++) {
+            char plan = plans[i].charAt(0);
+
+            int nx = -1, ny = -1;
+            for(int j=0; j<4; j++) {
+                if(plan == moveTypes[j]) {
+                    nx = x + dx[j];
+                    ny = y + dy[j];
+                }
+            }
+
+            if(nx < 1 || ny < 1 || nx > n || ny > n) continue;
+
+            x = nx;
+            y = ny;
+
+        }
+        System.out.println(x + " " + y);
     }
 }
