8e8009fc93a93e8b58ec2c795770d921ab09a90d
OOHYEE <azss0470@gmail.com>
2021-08-14 22:38:20 +0900

    [Implementation] 이코테 시각 솔루션 추가

--- java/src/Implementation/Ex4_2.java
+++ java/src/Implementation/Ex4_2.java
@@ -3,7 +3,6 @@ package Implementation;
 import java.util.Scanner;
 
 public class Ex4_2 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -19,5 +18,28 @@ public class Ex4_2 {
         }
 
         System.out.println(count);
+    }
+}
+
+class Solution4_2 {
+
+    public static boolean check(int h, int m, int s) {
+        return h % 10 == 3 || m / 10 == 3 || m % 10 == 3 || s / 10 == 3 || s % 10 == 3;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int cnt = 0;
+
+        for(int i=0; i<= h; i++) {
+            for(int j=0; j<60; j++) {
+                for(int k=0; k<60; j++) {
+                    if(check(i,j,k)) cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
     }
 }
