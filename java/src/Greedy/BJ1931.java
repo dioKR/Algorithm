a85f93600f6418d3123d571139051962167c4476
OOHYEE <azss0470@gmail.com>
2021-08-14 19:42:02 +0900

    [Greedy] 백준1931 : 회의실배정 문제풀이
    종료시간이 같을 경우 정렬기준을 잡아놓지않았다.
    종료시간이 같을 경우 시작 시간이 빠른순으로 정렬하는 코드를 추가

--- java/src/Greedy/BJ1931.java
+++ java/src/Greedy/BJ1931.java
@@ -1,2 +1,56 @@
-package Greedy;public class BJ1931 {
+package Greedy;
+
+import java.util.*;
+
+class meeting implements  Comparable<meeting>{
+    int start;
+    int end;
+
+    meeting(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public int compareTo(meeting m) {
+        // 종료시간이 같을 경우 시작시간이 빠른순으로 정렬해야함
+        if(end == m.end) {
+            return start - m.start;
+        }
+        return end - m.end;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + start + "," + end + ")";
+    }
+
+}
+
+public class BJ1931 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = 0;
+
+        ArrayList<meeting> list = new ArrayList<meeting>(n);
+
+        for(int i=0; i<n; i++) {
+            int start = sc.nextInt(); int end = sc.nextInt();
+            list.add(new meeting(start, end));
+        }
+
+        Collections.sort(list);
+
+        int end_time = 0;
+        for(int i=0; i<n; i++) {
+            meeting m = list.get(i);
+            if(end_time <= m.start) {
+                end_time = m.end;
+                result++;
+            }
+        }
+
+        System.out.print(result);
+    }
 }
