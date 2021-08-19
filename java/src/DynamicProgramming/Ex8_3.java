ba546503dbe07b6abbb201c2415e076f8caf221c
OOHYEE <azss0470@gmail.com>
2021-08-20 00:12:32 +0900

    [DP] 피보나치 수열 (Bottom-Up)

---
+++ java/src/DynamicProgramming/Ex8_3.java
@@ -0,0 +1,19 @@
+package DynamicProgramming;
+
+public class Ex8_3 {
+
+    static long[] d = new long[100];
+
+    public static void main(String[] args) {
+        d[1] = 1;
+        d[2] = 1;
+        int n = 50;
+
+        for(int i=3 ;i<=n; i++) {
+            d[i] = d[i-1] + d[i-2];
+        }
+
+        System.out.println(d[50]);
+
+    }
+}
