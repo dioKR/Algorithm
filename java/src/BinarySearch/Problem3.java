b4117797f1b0918227d58060b8cbb0b1b5ae0679
OOHYEE <azss0470@gmail.com>
2021-08-19 23:36:44 +0900

    [BinarySearch] 떡볶이 떡 만들기 문제풀이

---
+++ java/src/BinarySearch/Problem3.java
@@ -0,0 +1,53 @@
+package BinarySearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 떡볶이 떡 만들기
+public class Problem3 {
+
+    static int n;
+    static int m;
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        int[] dducks = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dducks[i] = sc.nextInt();
+        }
+
+        Arrays.sort(dducks);
+        System.out.println(Arrays.toString(dducks));
+
+        int result = binary_search(dducks, m, 0, dducks[n - 1]);
+        System.out.println(result);
+
+    }
+
+    public static int binary_search(int[] arr, int target, int start, int end) {
+        int result = 0;
+        while(start <= end) {
+            int total = 0;
+            int mid = (start + end) / 2;
+            for (int j : arr) {
+                if (j > mid) {
+                    total += (j - mid);
+                }
+            }
+
+            if(total < target) end = mid-1;
+            else {
+                result = mid;
+                start = mid+1;
+            }
+
+        }
+        return result;
+    }
+
+
+}
