aa9d832f4eac8e761d2d6567978ff3903a641980
OOHYEE <azss0470@gmail.com>
2021-09-15 10:27:09 +0900

    [구현] 백준 11866 : 요세푸스 문제 0 문제풀이

--- java/src/Implementation/BAEKJOON/BJ11866.java
+++ java/src/Implementation/BAEKJOON/BJ11866.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
+// 요세푸스 문제 0
 public class BJ11866 {
 
 
@@ -15,47 +16,7 @@ public class BJ11866 {
         int k = sc.nextInt();
 
 
-        for(int i=1; i<=n; i++) {package Implementation.BAEKJOON;
-
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-
-            public class BJ11866 {
-
-
-                public static void main(String[] args) {
-                    Scanner sc = new Scanner(System.in);
-                    Queue<Integer> q = new LinkedList<Integer>();
-
-                    int n = sc.nextInt();
-                    int k = sc.nextInt();
-
-
-                    for(int i=1; i<=n; i++) {
-                        q.offer(i);
-                    }
-
-                    int cnt = 1;
-                    System.out.print("<");
-                    while(!q.isEmpty()) {
-                        int num = q.poll();
-                        if(cnt == k) {
-                            System.out.print(num);
-                            if(!q.isEmpty()) System.out.print(", ");
-                            cnt = 1;
-                            continue;
-                        } else {
-                            q.offer(num);
-                        }
-                        cnt++;
-                    }
-                    System.out.print(">");
-
-
-                }
-            }
-
+        for(int i=1; i<=n; i++) {
             q.offer(i);
         }
 
