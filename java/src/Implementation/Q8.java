54f72339f446f9108bfe0f7a458a16a48f8072fd
OOHYEE <azss0470@gmail.com>
2021-09-24 23:19:52 +0900

    [구현] 이코테 유형별 기출문제 : 문자열 재정렬 문제풀이

--- java/src/Implementation/Q8.java
+++ java/src/Implementation/Q8.java
@@ -1,5 +1,31 @@
 package Implementation;
 
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 // 문자열 재정렬
 public class Q8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int sum = 0;
+        PriorityQueue<Character> alpha = new PriorityQueue<Character>();
+
+        for(int i=0; i<str.length(); i++) {
+            int num = (str.charAt(i) - '0');
+            if(0 <= num && num <= 9) {
+                sum += num;
+            } else {
+                alpha.offer(str.charAt(i));
+            }
+        }
+
+        while(!alpha.isEmpty()) {
+            System.out.print(alpha.poll());
+        }
+
+        System.out.print(sum);
+
+
+    }
 }
