aa37929e1fb9a0f22793733484057af2b826bd42
OOHYEE <azss0470@gmail.com>
2021-10-15 13:11:43 +0900

    [Sorting] 백준 1427 : 소트인사이드 문제풀이

---
+++ java/src/Sorting/BAEKJOON/BJ1427.java
@@ -0,0 +1,40 @@
+package Sorting.BAEKJOON;
+
+import java.util.Scanner;
+
+// 소트인사이드
+public class BJ1427 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int[] num = new int[str.length()];
+
+        for(int i=0; i<str.length(); i++) {
+            num[i] = str.charAt(i) - '0';
+        }
+
+        for(int i=0; i<str.length()-1; i++) {
+            int max = num[i];
+            int index= i;
+            for(int j=i+1; j<str.length(); j++) {
+                if(num[j] > max) {
+                    max = num[j];
+                    index = j;
+                }
+            }
+
+            int tmp = num[i];
+            num[i] = num[index];
+            num[index] = tmp;
+
+        }
+
+        for (int j : num) {
+            System.out.print(j);
+        }
+
+    }
+}
