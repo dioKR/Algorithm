129ee0696dc4f0e61dfe2306329243b42a5fc1b1
OOHYEE <azss0470@gmail.com>
2021-10-19 11:35:07 +0900

    [Sorting] 백준 2309 : 일곱 난쟁이 문제풀이

---
+++ java/src/Sorting/BAEKJOON/BJ2309.java
@@ -0,0 +1,42 @@
+package Sorting.BAEKJOON;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 일곱 난쟁이
+public class BJ2309 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[9];
+        int sum = 0;
+
+        for(int i=0; i<9; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+        for(int i=0; i<9; i++) {
+            for(int j=i+1; j<9; j++) {
+                if(sum - arr[i] - arr[j] == 100) {
+                    for(int k=0; k<9; k++) {
+                        if(i==k || j==k) {
+                            continue;
+                        }
+                        System.out.println(arr[k]);
+                    }
+                    System.exit(0);
+                }
+            }
+        }
+
+
+
+
+
+
+
+    }
+
+}
