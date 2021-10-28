1c431d4605efd253b2a37939672f34c068cd7807
OOHYEE <azss0470@gmail.com>
2021-10-28 23:51:48 +0900

    연습장

---
+++ java/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static boolean[] check;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        check = new boolean[n+1];
+
+        int index = 1;
+
+        for(int i=0; i<n; i++) {
+
+        }
+
+
+    }
+}
---
+++ javascript/4.Sorting/main.js
@@ -0,0 +1,7 @@
+{
+  let str = "3";
+  console.log(str[0]);
+  console.log(str[1]);
+
+  console.log(str[0] < str[1]);
+}
