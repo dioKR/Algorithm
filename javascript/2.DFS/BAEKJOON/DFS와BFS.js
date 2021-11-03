83b5e2727fe357daf31a6f93986a1c6ea29c897c
OOHYEE <azss0470@gmail.com>
2021-11-03 10:08:49 +0900

    [BAEKJOON] 백준 1260 : DFS와 BFS문제풀이

--- javascript/2.DFS/BAEKJOON/DFS와BFS.js
+++ javascript/2.DFS/BAEKJOON/DFS와BFS.js
@@ -6,7 +6,7 @@
   const fs = require("fs");
 
   let input = fs.readFileSync("./input.txt").toString().trim();
-  console.log(input);
+
   input = input.split("\n");
   let arr = input[0].split(" ").map((i) => +i);
 
@@ -59,10 +59,95 @@
     }
   }
 
-  dfs(v);
-  console.log(result.join(" "));
-  visited.fill(false);
-  result = [];
-  bfs(v);
-  console.log(result.join(" "));
+  // dfs(v);
+  // console.log(result.join(" "));
+  // visited.fill(false);
+  // result = [];
+  // bfs(v);
+  // console.log(result.join(" "));
+}
+
+{
+  const input = [];
+  let graph, visited, result;
+
+  const strToNumArr = (str) =>
+    str.split(" ").map((numString) => Number(numString));
+
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on("line", (line) => {
+    input.push(line.trim());
+  }).on("close", () => {
+    const [n, m, v] = strToNumArr(input.shift());
+    graph = [...Array(n + 1)].map(() => []);
+    visited = [...Array(n + 1)].fill(false);
+    let v1, v2;
+    input.forEach((str) => {
+      [v1, v2] = strToNumArr(str);
+      insertEdge(v1, v2);
+      insertEdge(v2, v1);
+    });
+
+    result = [];
+    dfs(v);
+    console.log(result.join(" "));
+
+    visited.fill(false);
+    result = [];
+    bfs(v);
+    console.log(result.join(" "));
+  });
+
+  const insertEdge = (vFront, vBack) => {
+    let index;
+    for (index = 0; index < graph[vFront].length; index++) {
+      if (graph[vFront][index] < vBack) {
+        continue;
+      }
+      if (graph[vFront][index] === vBack) {
+        index = null;
+      }
+      break;
+    }
+    if (index !== null) {
+      graph[vFront].splice(index, 0, vBack);
+    }
+  };
+
+  const dfs = (v) => {
+    if (visited[v]) {
+      return;
+    }
+
+    visited[v] = true;
+    result.push(v);
+    graph[v].forEach((vertex) => {
+      if (!visited[vertex]) {
+        dfs(vertex);
+      }
+    });
+  };
+
+  const bfs = (vStart) => {
+    const willVisit = [vStart];
+    let v;
+    while (willVisit.length !== 0) {
+      v = willVisit.shift();
+      if (visited[v]) {
+        continue;
+      }
+      visited[v] = true;
+      result.push(v);
+      graph[v].forEach((vertex) => {
+        if (!visited[vertex]) {
+          willVisit.push(vertex);
+        }
+      });
+    }
+  };
 }
