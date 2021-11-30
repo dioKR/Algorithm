5a2c9877db1f394f0cdb380f5bb75ff6811b9636
OhYeHwan <azss0470@gmail.com>
2021-12-01 01:06:05 +0900

    [Syntax] list

---
+++ python/syntax/8.list2.py
@@ -0,0 +1,33 @@
+# 리스트와 내장함수 (2)
+
+a = [23, 12, 36, 53, 19]
+print(a[:3])  # 슬라이스
+print(a[1:4])
+print(len(a))
+
+
+for i in range(len(a)):
+    print(a[i], end=" ")
+print()
+
+for x in a:
+    print(x, end=' ')
+print()
+
+for x in enumerate(a):  # tuple로 묶어줌
+    print(x)
+print()
+
+b = (1, 2, 3, 4, 5)  # tuple => 값 변경 불가능!
+
+
+for index, value in enumerate(a):
+    print(index, value)
+print()
+
+
+# all , any
+if all(60 > x for x in a):
+    print("yes")
+else:
+    print("no")
