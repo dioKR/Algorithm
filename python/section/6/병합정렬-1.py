0e309de5e7b12a041a501361a3bdd69f22068b7f
OOHYEE <azss0470@gmail.com>
2021-12-20 21:57:29 +0900

    [Section-6]

---
+++ python/section/6/병합정렬-1.py
@@ -0,0 +1,14 @@
+# 병합정렬
+def Dsort(lt, rt):
+    if lt < rt:
+        
+
+
+if __name__ == "__main__":
+    arr = [23,11,45,36,15,67,33,21]
+    print("Before sort : ", end="")
+    print(arr)
+    Dsort(0,7)
+    print()
+    print("After sort : ", end='')
+    print(arr)
