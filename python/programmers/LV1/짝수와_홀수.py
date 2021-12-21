015d755fe947c2b1e19089a148255934fd590eea
OhYeHwan <azss0470@gmail.com>
2021-12-22 00:15:09 +0900

    [프로그래머스] LV1 : 짝수와 홀수 문제풀이

---
+++ python/programmers/LV1/짝수와_홀수.py
@@ -0,0 +1,13 @@
+# 나의 풀이
+def solution(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# 다른 사람 풀이
+def solution2(num):
+    if (num%2):
+        return "Odd"
+    else:
+        return "Even"
