a21241029e5bcfbefb049ad5dfe8ebd5fc0c2568
OhYeHwan <azss0470@gmail.com>
2021-12-28 09:34:53 +0900

    [프로그래머스] LV1 : 약수의 합 문제풀이

---
+++ python/programmers/LV1/약수의_합.py
@@ -0,0 +1,12 @@
+n = 12
+
+def solution(n):
+    answer = 0
+    for i in range(1,n+1):
+        if n%i==0:
+            answer += i
+    return answer
+
+
+def sumDivisor(n):
+    return n + sum([i for i in range(1, (n//2) + 1) if n % i == 0])
