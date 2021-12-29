51bbf0c749c76c85060577ca269436a515b60403
OhYeHwan <azss0470@gmail.com>
2021-12-29 19:37:15 +0900

    [프로그래머스] LV1 : 소수찾기 문제풀이

---
+++ python/programmers/LV1/소수_찾기.py
@@ -0,0 +1,19 @@
+# 에레토스테네스의 체
+
+def solution(n):
+    answer = []
+    arr = [0] * (n+1)
+
+    for i in range(2, n+1):
+        if arr[i] == 0:
+            answer.append(arr[i])
+        for j in range(i, n+1, i):
+            arr[j] = 1
+    return len(answer)
+
+def solution2(n):
+    num = set(range(2,n+1))
+    for i in range(2, n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
