a824b8530213ef95a51e8d63de844de285250700
OhYeHwan <azss0470@gmail.com>
2022-01-10 09:39:29 +0900

    [이코테] BFS : 예제 작성

---
+++ python/이코테/BFS/BFS예제.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+visited = [False] * 9
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        cur = queue.popleft()
+        print(cur, end=" ")
+        for i in graph[cur]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+bfs(graph, 1, visited)
