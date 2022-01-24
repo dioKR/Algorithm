77081b9d3b8791bd0193a213e0adc28879d2c911
OhYeHwan <azss0470@gmail.com>
2022-01-24 15:45:45 +0900

    [프로그래머스] LV2 : 프린터 문제풀이

--- python/programmers/LV2/프린터.py
+++ python/programmers/LV2/프린터.py
@@ -1,7 +1,7 @@
 from collections import deque
+import enum
 
 def solution(priorities, location):
-    
     answer = 0
     p = deque()
     
@@ -11,9 +11,22 @@ def solution(priorities, location):
     while p:
         cur = p.popleft()
         
-        if cur[0] == location and all(cur[1] > a[1] for a in p):
-            return (answer-1)
+        if cur[0] == location and all(cur[1] >= a[1] for a in p):
+            answer += 1
+            return answer
+        elif cur[0] != location and all(cur[1] >= a[1] for a in p):
+            answer += 1
         else:
-            answer += 1    
+            p.append(cur)    
     
-    return (answer-1)
+def solution2(priorities, location):
+    queue = [(i,p) for i,p in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
