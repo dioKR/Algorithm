33b843a54cfb5b8a4d20dfa4aca6ec1488f30dbb
yehwan_oh <yehwan_oh@tmax.co.kr>
2022-03-15 09:48:58 +0900

    [InterView] 이진탐색 : Binary Search 문제풀이

---
+++ python/interview/이진탐색/Binary_Search.py
@@ -0,0 +1,34 @@
+# 리트코드 704
+# 이진탐색
+
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
+    
+    def search2(self, nums: List[int], target: int) -> int:
+        def binary_search(left, right):
+            if left <= right:
+                mid = (left + right) // 2
+
+                if nums[mid] < target:
+                    return binary_search(mid+1, right)
+                elif nums[mid] > target:
+                    return binary_search(left, mid-1)
+                else:
+                    return mid
+            else:
+                return -1
+        return binary_search(0, len(nums)-1)
