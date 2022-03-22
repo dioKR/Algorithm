0223b206e9b8840385d5c86023c576a560529a04
OhYeHwan <azss0470@gmail.com>
2022-03-23 00:01:08 +0900

    [그래프] 상태트리 : 순열 추측하기 문제풀이

---
+++ python/interview/그래프/상태트리/순열_추측하기.py
@@ -0,0 +1,25 @@
+import sys
+
+n = 4
+f = 16
+
+def DFS(L, sum):
+    if L==n and sum==f:
+        for x in p:
+            print(x, end=" ")
+        sys.exit(0)
+    else:
+        for i in range(1, n+1):
+            if ch[i] == 0:
+                ch[i] = 1
+                p[L] = i
+                DFS(L+1, sum + (p[L] * b[L]))
+                ch[i] = 0
+
+p = [0] * n
+b = [1] * n
+ch = [0] * (n+1)
+
+for i in range(1,n):
+    b[i] = (b[i-1] * (n-i)) // i
+DFS(0,0)
